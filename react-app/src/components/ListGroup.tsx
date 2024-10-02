7c18ab94c05ee5e4b392db77766275386e0046e5
Lisa Susheel <lisasusheel12@gmail.com>
2024-10-02 23:23:46 +0530

    Dynamically fetching data from a list

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,17 +1,24 @@
 import { Fragment } from "react";
 
+const items = [
+    'New York',
+    'Paris',
+    'Woff',
+    'Chirp'
+]
+
+
 function ListGroup()
 {
-    return <Fragment>
+    return <>
         <h1>List</h1>
         <ul className="list-group">
-    <li className="list-group-item active" aria-current="true">An active item</li>
-    <li className="list-group-item">A second item</li>
-    <li className="list-group-item">A third item</li>
-    <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth one</li>
+            
+        {items.map((item) => (
+            <li key = {item}>{item}</li>
+            ))}  
   </ul>
-  </Fragment>
+  </>
 }
 
 export default ListGroup; //export this onto App
