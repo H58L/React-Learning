8a7ad1dff27b8f6e5013b5260678cbd6ba78f663
Lisa Susheel <lisasusheel12@gmail.com>
2024-10-02 21:05:02 +0530

    First very basic react app

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,12 @@
+import { Component } from "react"
+
+// Ways to creat a react componenet
+// 1. Function based Component
+// 2. Class componenet
+
+//Use pAscal casing
+function Message() {
+return <h1>Hello World</h1>  //JSX - Jaascript XML is this syntax
+}
+
+export default Message; //export the function to be able to use it in another componenet 
