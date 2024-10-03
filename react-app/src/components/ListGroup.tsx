ae97917445a7875996b3b7ca7e46e3285608b7c3
Lisa Susheel <lisasusheel12@gmail.com>
2024-10-03 10:25:14 +0530

    using constant to reder inside JSX

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -8,14 +8,14 @@ let items = [
 ]
 
 items = []
-
+const message = items.length === 0 ? <p>Not item found</p> : null
 
 function ListGroup()
 {
        
     return <>
         <h1>List</h1>
-        {items.length === 0 ? <p>Not item found</p> : null}  
+        {message}  
         <ul className="list-group">
             
         {items.map((item) => (
